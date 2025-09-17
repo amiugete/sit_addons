17c850747f12d950e1f510401c6c7389e386a0e2
RobiFag <robifagandini@hotmail.it>
2025-09-17 11:50:27 +0200

    fix bug on date after moving db

--- dettagli_percorso_modal.php
+++ dettagli_percorso_modal.php
@@ -704,7 +704,7 @@ join anagrafe_percorsi.cons_mapping_uo cmu on cmu.id_uo = pu.id_ut
 left join elem.automezzi a on a.cdaog3 = pu.cdaog3 
 join elem.squadre s on s.id_squadra = pu.id_squadra 
 join topo.ut u on u.id_ut = cmu.id_uo_sit  
-where pu.cod_percorso = $1  and pu.data_disattivazione = $2";
+where pu.cod_percorso = $1  and pu.data_disattivazione = to_date($2, 'DD/MM/YYYY')";
 
 // RIMESSA / SEDE OPERATIVA
 $query_rimessa=$query0_modal ." and rimessa = 'S' and responsabile = 'N'";
@@ -1005,7 +1005,7 @@ if($check_versione_successiva==0){
   from topo.ut   
   where id_ut not in (select distinct cmu.id_uo_sit  from anagrafe_percorsi.percorsi_ut pu 
 join anagrafe_percorsi.cons_mapping_uo cmu on cmu.id_uo = pu.id_ut 
-where cod_percorso = $1 and data_disattivazione  =$2)
+where cod_percorso = $1 and data_disattivazione  =to_date($2, 'DD/MM/YYYY'))
   order by descrizione  ;";
   
   $result3 = pg_prepare($conn, "query3", $query3);
